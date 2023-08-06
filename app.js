593ec361daba4d970074699846223b0b193299a9
Daniil Bykh <109330484+DaniilRen@users.noreply.github.com>
2023-08-06 12:20:20 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const input_1 = document.getElementById('input-1')
+const input_2 = document.getElementById('input-2')
+const select_1 = document.getElementById('select-1')
+const select_2 = document.getElementById('select-2')
+const know_currency_btn = document.querySelector('.know-currency')
+
+
+know_currency_btn.addEventListener('click', know_currency) 
+
+
---
+++ functions.js
@@ -0,0 +1,17 @@
+const getJson = async () => {
+  let select_1_value = select_1.value
+  let select_2_value = select_2.value
+  let input_value = input_1.value
+
+  const response = await fetch(`https://api.getgeoapi.com/v2/currency/convert?api_key=ed36c33db0b790c7b75b5e6b4100b7302269995c&from=${select_1_value}&to=${select_2_value}&amount=${input_value}&format=json`);
+  const json = await response.json();
+  output = json.rates[select_2_value]['rate_for_amount']
+
+  input_2.value = output
+}
+
+
+function know_currency() {
+
+  let data = getJson()
+}
